3df4a5ca95ce527bfae765d68d338255b5b2b5ce
satyam prajapati <skpckt56@email.com>
2025-12-22 21:22:37 +0530

    renames

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -1,11 +1,15 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:MySQLsatyam8.04@localhost:3306/todo_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
